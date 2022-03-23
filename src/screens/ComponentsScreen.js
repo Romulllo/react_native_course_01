44e93273f525f7189c232cef8e365d054ae11fe6
Romulo Santos <romulosantos88@hotmail.com>
2022-03-23 08:06:13 -0300

    lesson 12

---
+++ src/screens/ComponentsScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import{ Text, StyleSheet } from `react-native`;
+ 
+const ComponentsScreen = () => {
+  return <Text style={style.texStyle}>This is the components screen</Text>
+};
+
+const style = StyleSheet.create({
+  texStyle: {
+    fontSize: 30
+  }
+});
+
+export default ComponentsScreen
