c6d4692c69858ec4917c18b4e3920c2063adbd8e
Romulo Santos <romulosantos88@hotmail.com>
2022-03-23 08:27:03 -0300

    lesson 13

--- src/screens/ComponentsScreen.js
+++ src/screens/ComponentsScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import{ Text, StyleSheet } from `react-native`;
+import { Text, StyleSheet } from 'react-native';
  
 const ComponentsScreen = () => {
   return <Text style={style.texStyle}>This is the components screen</Text>
